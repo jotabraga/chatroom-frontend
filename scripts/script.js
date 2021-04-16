b013d00f916d1382c3cc33209acb8bd183940a06
jotabraga <jpmbraga.feg@gmail.com>
2021-04-15 23:44:11 -0300

    Implementação da diferenciação das mensagens de status e privadas

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ let chatContent = [];
 let userName;
 
 function getInChat(){
+
     const nameInput = document.querySelector(".user-name");
     const name = nameInput.value;
     userName = name;
@@ -9,7 +10,6 @@ function getInChat(){
     const chatName = {
         name: name
     }
-
     const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants",chatName);
 
     promisse.then(startChat);
@@ -19,9 +19,10 @@ function getInChat(){
 function startChat(){
     chatInit();
     changeScreen();
-    setInterval(updateStatus,4000);
+    setInterval(updateStatus,3000);
     setInterval(chatInit,3000);
 }
+
 function updateStatus() {
     let message = {name: userName};     
     axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', message); 
@@ -48,23 +49,31 @@ function chatInit(){
     const promisseChat = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
     promisseChat.then(getChatData);
 }  
+
 function getChatData(answer){
     chatContent = answer.data;
     console.log(chatContent);
     renderChatMessages(); 
 } 
 
-function renderChatMessages(){
-    
+function renderChatMessages(){    
     
     const container = document.querySelector(".chat-content");
     container.innerHTML = "";
 
     for(let i=0; i < chatContent.length; i++)
     {
+        if(chatContent[i].type === "status"){
         container.innerHTML += `
-        <div class="chat-box">${chatContent[i].time} &nbsp; <strong>${chatContent[i].from}</strong> &nbsp; para ${chatContent[i].to}: ${chatContent[i].text}</div>
-    `;
+        <div class="chat-box status-type">${chatContent[i].time} &nbsp; <strong>${chatContent[i].from}</strong> &nbsp; para ${chatContent[i].to}: ${chatContent[i].text}</div>
+      `;}else    
+        if(chatContent[i].type === "private_message"){
+            container.innerHTML += `
+            <div class="chat-box private-type">${chatContent[i].time} &nbsp; <strong>${chatContent[i].from}</strong> &nbsp; para ${chatContent[i].to}: ${chatContent[i].text}</div>
+      `;}else{
+            container.innerHTML += `
+            <div class="chat-box">${chatContent[i].time} &nbsp; <strong>${chatContent[i].from}</strong> &nbsp; para ${chatContent[i].to}: ${chatContent[i].text}</div>
+      `;}    
     }
     window.scrollTo(0, document.body.scrollHeight);
 }
