46ad4944dff0dbd7f7471c2782f9cbb5c031f60b
jotabraga <jpmbraga.feg@gmail.com>
2021-07-08 02:15:32 -0300

    End of refactoring

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,34 @@
 let chatContent = [];
 let userName;
 
-function getInChat(){
+function getInChat() {
     const nameInput = document.querySelector(".user-name");
     const name = nameInput.value;
     userName = name;
 
-    const chatName = {name: name}
-    const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants",chatName);
+    const chatName = { name: name }
+    const promisse = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants", chatName);
 
     promisse.then(startChat);
     promisse.catch(changeName);
 }
-function changeName(){
-    const msgError = "O nome " +userName+ " está em uso, escolha outro para entrar!";
+function changeName() {
+    const msgError = "O nome " + userName + " está em uso, escolha outro para entrar!";
     alert(msgError);
 }
-function startChat(){
+function startChat() {
     chatInit();
     changeScreenFromEntranceToChatScreen();
-    setInterval(updateStatusToOnline,3000);
-    setInterval(chatInit,3000);
+    setInterval(updateStatusToOnline, 3000);
+    setInterval(chatInit, 3000);
 }
-function updateStatusToOnline(){
-    let message = {name: userName};     
-    axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', message); 
+function updateStatusToOnline() {
+    const message = { name: userName };
+    axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', message);
 }
 
-function changeScreenFromEntranceToChatScreen(){
-    const faceScreenDiv = document.querySelector(".entrance-screen");    
+function changeScreenFromEntranceToChatScreen() {
+    const faceScreenDiv = document.querySelector(".entrance-screen");
     const headerDiv = document.querySelector(".header");
     const chatContentDiv = document.querySelector(".chat-content");
     const footerDiv = document.querySelector(".footer");
@@ -38,56 +38,58 @@ function changeScreenFromEntranceToChatScreen(){
     chatContentDiv.classList.remove("hidden");
     footerDiv.classList.remove("hidden");
 }
-function chatInit(){
-    const promisseChat = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
-    promisseChat.then(getChatData);
+function chatInit() {
+    const promiseChat = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
+    promiseChat.then(getChatData);
 }
-function getChatData(answer){
+function getChatData(answer) {
     chatContent = answer.data;
     console.log(chatContent);
-    renderChatMessages(); 
+    renderChatMessages();
 }
-function renderChatMessages(){      
+function renderChatMessages() {
     const container = document.querySelector(".chat-content");
     container.innerHTML = "";
 
-    for(let i=0; i < chatContent.length; i++){
+    for (let i = 0; i < chatContent.length; i++) {
         let chatBoxStyleByType = "chat-box";
-        if(chatContent[i].type === "status"){
-            chatBoxStyleByType += " status-type";}
-        if(chatContent[i].type === "private_message"){
-            chatBoxStyleByType += " private-type";}
+        if (chatContent[i].type === "status") {
+            chatBoxStyleByType += " status-type";
+        }
+        if (chatContent[i].type === "private_message") {
+            chatBoxStyleByType += " private-type";
+        }
 
         container.innerHTML += `
         <div class="${chatBoxStyleByType}">${chatContent[i].time} &nbsp; <strong>${chatContent[i].from}</strong> &nbsp; para &nbsp; <strong>${chatContent[i].to}</strong>: ${chatContent[i].text}</div>
-      `;    
+      `;
     }
     window.scrollTo(0, document.body.scrollHeight);  //Essa função da um scroll automatico cada vez que um item é add ao chat ou o chat é atualizado
 }
 function sendMessage() {
     let textToSend = document.querySelector(".text-to-send");
     let textThatWillBeSended = textToSend.value;
-    
+
     const userMessageToChat = {
         from: userName,
         to: "Todos",
         text: textThatWillBeSended,
         type: "message"
-    }    
-    const requisition = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages",userMessageToChat);
-    requisition.then(chatInit); 
+    }
+    const requisition = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages", userMessageToChat);
+    requisition.then(chatInit);
     textToSend.value = "";
-    requisition.catch(errorFromSendMessageThreat);   
+    requisition.catch(errorFromSendMessageThreat);
 }
-function sendMessagePressingEnterButton(enterButton){
-    if(event.key === 'Enter'){
+function sendMessagePressingEnterButton(enterButton) {
+    if (event.key === 'Enter') {
         sendMessage();
     }
 }
-function errorFromSendMessageThreat(){
+function errorFromSendMessageThreat() {
     window.location.reload();
 }
-function showContactsToMessage(){
+function showContactsToMessage() {
     const darkenedBackground = document.querySelector(".darkened-background");
     const contactsList = document.querySelector(".contacts-list");
     console.log(contactsList);
@@ -95,7 +97,7 @@ function showContactsToMessage(){
     darkenedBackground.classList.remove("hidden");
     contactsList.classList.remove("hidden");
 }
-function hideContactsToMessage(element){
+function hideContactsToMessage(element) {
     this.classList.add("hidden");
     this.nextSibling.classList.add("hidden");
 }
